fe2face3f0f99632707812f6e961520987dec975
Ivan Pesin <ivan.pesin@gmail.com>
2022-12-03 11:24:11 +0200

    style improvement

--- 2022/2022.03/sol.py
+++ 2022/2022.03/sol.py
@@ -8,14 +8,14 @@ with open(sys.argv[1]) as f:
 score = 0
 for l in data:
     a, b = l[:len(l)//2], l[len(l)//2:]
-    c = (set(a) & set(b)).pop()
+    c, = set(a) & set(b)
     score += string.ascii_letters.index(c) + 1
 
 print(f"Part 1: {score}")
 
 score = 0
 for i in range(0,len(data),3):
-    c = (set(data[i]) & set(data[i+1]) & set(data[i+2])).pop()
+    c, = set(data[i]) & set(data[i+1]) & set(data[i+2])
     score += string.ascii_letters.index(c) + 1
 
 print(f"Part 2: {score}")
