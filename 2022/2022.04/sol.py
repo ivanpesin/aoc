cba436ce22fd0e38adf2a0291969cc11c4048b3d
Ivan Pesin <ivan.pesin@gmail.com>
2022-12-04 14:30:19 +0200

    Day 4 solution

--- 2022/2022.04/sol.py
+++ 2022/2022.04/sol.py
@@ -5,18 +5,10 @@ import sys, re
 with open(sys.argv[1]) as f:
     data = [ [ int(b) for b in re.findall(r'\d+',l) ] for l in f ]
 
-score = 0
+p1 = p2 = 0
 for a,b,x,y in data:
-    if (a >= x and b <= y) or \
-       (x >= a and y <= b):
-        score += 1
+    if (a >= x and b <= y) or (x >= a and y <= b): p1 += 1
+    if (a <= x <= b) or (x <= a <= y):             p2 += 1
 
-print(f"Part 1: {score}")
-
-score = 0
-for a,b,x,y in data:
-    if (a <= x <= b) or \
-       (x <= a <= y):
-        score += 1
-
-print(f"Part 2: {score}")
+print(f"Part 1: {p1}")
+print(f"Part 2: {p2}")
