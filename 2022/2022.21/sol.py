9ac7d9de20ac4c3f05c32e2f4447714b180c2468
Ivan Pesin <ivan.pesin@gmail.com>
2022-12-21 20:53:15 +0200

    Day 21

--- 2022/2022.21/sol.py
+++ 2022/2022.21/sol.py
@@ -3,32 +3,28 @@
 import sys
 
 with open(sys.argv[1]) as f:
-    data = { s.strip().split(': ')[0]:s.strip().split(': ')[1] for s in f }
+    data = dict(tuple(s.strip().split(': ')) for s in f)
 
 def monkey(m):
-    assert m in data
-    if data[m].isdigit(): return int(data[m])
-    
+    if data[m].isdigit(): return int(data[m])    
     a,op,b = data[m].split()
     return eval(f"{monkey(a)} {op} {monkey(b)}")
 
 print("Part 1:", monkey(sys.argv[2]))
 
-a, op, b = data['root'].split()
-target = monkey(b)
-
+start, op, target = data['root'].split()
 if len(sys.argv) > 3:
     data['humn'] = sys.argv[3]
-    print(f"a={monkey(a)} b={monkey(b)}")
+    print(f"a={monkey(start)} b={monkey(target)}")
 else:
     def bsearch(v,lo,hi):
         if hi < lo: return -1
 
         mid = (hi+lo)//2
         data['humn'] = str(mid)
-        m = monkey(a)
+        m = monkey(start)
         if m == v: return mid
         if m > v: return bsearch(v,mid+1,hi)
         return bsearch(v,lo,mid-1)
 
-    print("Part 2:", bsearch(monkey(b),1000000000000,10000000000000))
+    print("Part 2:", bsearch(monkey(target),1000000000000,10000000000000))
