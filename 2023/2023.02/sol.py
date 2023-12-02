589bbff91584d426b28d08550be4edfb79629908
Ivan Pesin <ivan.pesin@gmail.com>
2023-12-02 10:00:25 +0200

    2023 day 2

--- 2023/2023.02/sol.py
+++ 2023/2023.02/sol.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
-import sys, pprint, math
+import sys, pprint, math, collections
 
 data = [ s.strip() for s in open(sys.argv[1]).readlines() ]
 
 constraints = { 'red': 12, 'green': 13, 'blue': 14 }
 game, possible, powers = [], [], []
 for i, s in enumerate(data):
-    game.append({})
+    game.append(collections.defaultdict(list))
     for draw in s.split(': ')[1].split('; '):
         for cubes in draw.split(', '):
             n, color = cubes.split(' ')
-            if color not in game[i]: game[i][color] = []
             game[i][color].append(int(n))
 
     if all(max(game[i][color]) <= constraints[color] for color in game[i]): possible.append(i+1)
