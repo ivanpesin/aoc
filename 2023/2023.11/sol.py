81359030d3e01f20236e2bdc097df950b8942cc6
Ivan Pesin <ivan.pesin@gmail.com>
2023-12-11 14:22:03 +0200

    Day 11

--- 2023/2023.11/sol.py
+++ 2023/2023.11/sol.py
@@ -2,7 +2,7 @@
 
 import sys, pprint, itertools, time
 
-grid, a = [ list(s.strip()) for s in open(sys.argv[1]) ], []
+grid = [ list(s.strip()) for s in open(sys.argv[1]) ]
 
 expand_rows = [ r for r,l in enumerate(grid) if all(ch == '.' for ch in l) ]
 expand_cols = [ c for c in range(len(grid[0])) if all(l[c] == '.' for l in grid) ]
@@ -17,5 +17,5 @@ def dist(a,b, space_multiplier):
              (sum([ r in row_range for r in expand_rows ]) +
               sum([ c in col_range for c in expand_cols ])) * (space_multiplier-1) )
 
-print("Part 1:", sum([ dist(a,b,1) for a,b in pairs ]))
-print("Part 2:", sum([ dist(a,b,1_000_000) for a,b in pairs ]))
+print("Part 1:", sum([ dist(a,b,2) for a,b in pairs ]))
+print("Part 2:", sum([ dist(a,b,10) for a,b in pairs ]))
