5dde3a21f777608b832dedcd436bf8c5f604a2c2
Ivan Pesin <ivan.pesin@gmail.com>
2024-12-03 09:22:56 +0200

    aoc 2024.03

---
+++ 2024/2024.03/sol.py
@@ -0,0 +1,19 @@
+import sys, pprint, re
+
+data = open(sys.argv[1]).read()
+
+def solve(part2):
+    do, res = True, 0
+    for instr in re.findall(r"do\(\)|don't\(\)|mul\(\d+,\d+\)", data):
+        if instr == "do()":
+            do = True
+        elif instr == "don't()":
+            do = False
+        elif not part2 or (part2 and do):
+            a, b = map(int, re.findall(r'\d+', instr))
+            res += a * b
+
+    return res
+
+print("Part 1:", solve(False))
+print("Part 2:", solve(True))
