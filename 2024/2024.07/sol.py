c976494c1e3b4693cb3ef875ec396b456967e586
Ivan Pesin <ivan.pesin@gmail.com>
2024-12-07 09:10:27 +0200

    aoc 2024.07

---
+++ 2024/2024.07/sol.py
@@ -0,0 +1,28 @@
+import sys, pprint
+import itertools
+
+data = []
+for s in open(sys.argv[1]).readlines():
+    left, right = s.strip().split(': ')
+    right = right.split()
+    data.append((int(left), [int(x) for x in right]))
+
+def solve(part2=0):
+    res = 0
+    for test, nums in data:
+        ops = '+*|' if part2 else '+*'
+        for combination in itertools.product(ops, repeat=(len(nums)-1)):
+            r = nums[0]
+            for i, op in enumerate(combination):
+                if op == '+': r += nums[i+1]
+                if op == '*': r *= nums[i+1]
+                if op == '|': r = int(str(r) + str(nums[i+1]))
+
+            if r == test:
+                res += test
+                break
+    return res
+
+
+print("Part 1:", solve())
+print("Part 2:", solve(1))
