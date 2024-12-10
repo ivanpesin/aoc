fe3f4524a876da4c4e37198539b0540fe223c336
Ivan Pesin <ivan.pesin@gmail.com>
2024-12-10 10:42:12 +0200

    aoc 2024.10

--- 2024/2024.10/sol.py
+++ 2024/2024.10/sol.py
@@ -5,17 +5,16 @@ import sys, pprint, collections
 grid = { (x, y): cell for y, row  in enumerate(open(sys.argv[1]))
                       for x, cell in enumerate(row.strip())       }
 
-trailheads = [ (x, y) for x, y in grid if grid[x, y] == '0' ]
+trailheads = [ a for a in grid if grid[a] == '0' ]
 
 def bfs(start, part2):
-    queue = collections.deque([start])
-    visited = [start]
+    queue, visited = collections.deque([start]), [start]
     while queue:
         x, y = queue.popleft()
         for dx, dy in ((0, 1), (1, 0), (0, -1), (-1, 0)):
             if (x+dx, y+dy) in grid and \
-                (part2 or (x+dx, y+dy) not in visited) and \
-                int(grid[x+dx, y+dy]) - int(grid[x,y]) == 1:
+               (part2 or (x+dx, y+dy) not in visited) and \
+               int(grid[x+dx, y+dy]) - int(grid[x,y]) == 1:
                 queue.append((x+dx, y+dy))
                 visited.append((x+dx, y+dy))
     return visited
@@ -23,7 +22,7 @@ def bfs(start, part2):
 res1 = res2 = 0
 for trailhead in trailheads:
     res1 += sum(1 for a in bfs(trailhead,part2=False) if grid[a] == '9')
-    res2 += sum(1 for a in bfs(trailhead,part2=True) if grid[a] == '9')
+    res2 += sum(1 for a in bfs(trailhead,part2=True)  if grid[a] == '9')
 
 print("Part 1:", res1)
 print("Part 2:", res2)
