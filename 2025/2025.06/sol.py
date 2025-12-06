3cd6e416e9b7d3756fa605488949ff3148d35d31
Ivan Pesin <ivan.pesin@gmail.com>
2025-12-06 10:54:12 +0200

    2025.06

--- 2025/2025.06/sol.py
+++ 2025/2025.06/sol.py
@@ -12,14 +12,13 @@ print(f'Part 1: {res}')
 
 data = { (x,y): ch for y,s in enumerate(open(sys.argv[1]).readlines())
                    for x,ch in enumerate(s.strip('\n')) }
-
-max_x, max_y = max(x for x,y in data), max(y for x,y in data)
+max_x, max_y = max(x for x,_ in data), max(y for _,y in data)
 
 digits, nums, o, res = [], [], None, 0
 for x in range(max_x, -1, -1):
     for y in range(max_y+1):
-        if data.get((x,y), ' ').isdigit(): digits.append(data[x,y])
-        elif data.get((x,y), ' ') in '+*': o = op[data[x,y]]
+        if data.get((x,y), '').isdigit(): digits.append(data[x,y])
+        elif data.get((x,y), '') in '+*': o = op[data[x,y]]
     if not digits: continue
     nums.append(int(''.join(digits))); digits = []
     if o is not None:
