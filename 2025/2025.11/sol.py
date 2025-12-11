4ecdcb051c5d050bf39e691551ca24af94cf22a8
Ivan Pesin <ivan.pesin@gmail.com>
2025-12-11 17:37:16 +0200

    2025.11

---
+++ 2025/2025.11/sol.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys, pprint, functools
+
+net = {}
+for s in open(sys.argv[1]):
+    k, v = s.split()[0].strip(':'), s.split()[1:]
+    net[k] = v
+
+@functools.cache
+def dfs(node, end):
+    if node == end:
+        return 1
+    if node == 'out': return 0
+
+    return sum(dfs(n, end) for n in net[node])
+
+start, end = sys.argv[2].split(',')
+print(dfs(start, end))
+
+print(dfs('svr','fft') * dfs('fft','dac') * dfs('dac','out'))
+
+# p2 = 0
+# for p in paths:
+#     if 'fft' in p: p2 += 1
+# print('Part 2:', p2)
